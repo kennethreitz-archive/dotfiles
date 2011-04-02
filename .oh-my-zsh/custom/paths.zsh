67c16567d93809c8b7aa74940ff7364ef2ac60ac
Kenneth Reitz <me@kennethreitz.com>
2011-04-02 11:40:55 -0400

    misc paths

---
+++ .oh-my-zsh/custom/paths.zsh
@@ -0,0 +1,9 @@
+# use vim as an editor
+export EDITOR="mate -w"
+PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin/:$PATH"
+PATH="/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/local/sbin/:$PATH"
+PATH="/usr/local/homebrew/bin/:$PATH"
+PATH="/usr/local/homebrew/sbin/:$PATH"
+PATH="/usr/local/Cellar/python/2.7/Frameworks/Python.framework/Versions/2.7/bin/:$PATH"
+PATH="/usr/local/Cellar/python/2.7/bin:$PATH"
+PATH="/Developer/SDKs/android-sdk-mac_86/tools/:$PATH"
