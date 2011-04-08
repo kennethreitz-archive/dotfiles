42cca133660caee4d216a19cd9641c9e16952f94
Kenneth Reitz <me@kennethreitz.com>
2011-04-08 11:17:37 -0400

    oh my zsh update

---
+++ .oh-my-zsh/plugins/macports/macports.plugin.zsh
@@ -0,0 +1,12 @@
+#Aliases
+alias pc="sudo port clean --all installed"
+alias pi="sudo port install $1"
+alias psu="sudo port selfupdate"
+alias puni="sudo port uninstall inactive"
+alias puo="sudo port upgrade outdated"
+alias pup="psu && puo"
+
+# add macports completion function to path
+fpath=($ZSH/plugins/macports $fpath)
+autoload -U compinit
+compinit -i
